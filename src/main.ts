266dff2cefa91d0b4ac0e889282fe23ba0c3167d
ardszsantos <lokgamer01@gmail.com>
2024-10-23 16:05:19 -0300

    feat: setup swagger and enable cors

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple Blog')
+    .setDescription('The Simple Blog API documentation')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors()
+
+  await app.listen(3000);
+}
+bootstrap();
