d314980fa9e6d3b5e46c98d4024f12051c929ef9
ardszsantos <lokgamer01@gmail.com>
2024-10-23 16:04:48 -0300

    feat: created the app module

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from './config/database.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/user/user.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Allows the use of environment variables globally
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: getDatabaseConfig, // Uses the config function from database.config.ts
+    }),
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
