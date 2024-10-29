ba0c02e81fe6cad93a79a8a198b6ad13981b4f14
ardszsantos <lokgamer01@gmail.com>
2024-10-29 13:58:49 -0300

    feat: connected with notifications module

--- src/modules/comments/comments.module.ts
+++ src/modules/comments/comments.module.ts
@@ -1,4 +1,3 @@
-// src/modules/comments/comments.module.ts
 import { Module } from '@nestjs/common';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
@@ -6,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment/comment';
 import { Post } from '../post/entities/post/post';
 import { User } from '../user/entities/user/user';
+import { NotificationsModule } from '../notifications/notifications.module'; // Import this
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment, Post, User])],
+  imports: [TypeOrmModule.forFeature([Comment, Post, User]), NotificationsModule], // Add NotificationsModule here
   controllers: [CommentsController],
   providers: [CommentsService],
 })
